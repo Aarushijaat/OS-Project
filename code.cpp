a6510e13f08e8b33a650dca2535c5ad9f924872c
Aarushi <149956173+Aarushijaat@users.noreply.github.com>
2025-04-04 18:44:56 +0530

    Create code.cpp
    This C++ program simulates a bank account withdrawal system using POSIX threads (pthread) and mutex locking for synchronization. It ensures that multiple threads safely access the shared balance variable.
    The withdraw function locks the mutex before checking and updating balance to prevent race conditions.
    
    If sufficient balance exists, it withdraws the amount and prints the updated balance. Otherwise, it prints "Insufficient balance."
    
    usleep(100000) introduces a delay to simulate real-world transaction processing.
    
    The mutex is unlocked after the operation, allowing other threads to proceed.

---
+++ code.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<thread.h>
+#include<unistd.h>
+int balance = 1000;
+pthread_mutex_t lock;
+void* withdraw(void* arg) {
+    int amount = *(int*)arg;
+    pthread_mutex_lock(&lock);
+
+    if(balance>= amount) {
+        std::cout<<"Withdrawing "<<amount<< "....\n";
+        usleep(100000);
+        balance-= amount;
+        std::cout<<"Transaction successful! Remaining balance: "<<balance<< "\n";
+       }
+       else {
+        std::cout<<"Insufficient balance\n";
+       }
+       pthread_mutex_unlock(&lock);
+       return nullptr;
+}
